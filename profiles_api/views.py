f59b26e9c5b7ff2f0747eab568b14883d3d58e3a
Samiul <samiulislam@gmail.com>
2020-12-05 19:40:37 +0900

    Added login api

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -5,6 +5,8 @@ from rest_framework import status #list of HTTP status code, like 200, 400, 404 
 from rest_framework import viewsets
 from rest_framework.authentication import TokenAuthentication
 from rest_framework import filters
+from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.settings import api_settings
 
 from profiles_api import serializers #What data to expect when making post put and patch request
 from profiles_api import models
@@ -108,3 +110,8 @@ class UserProfileViewSet(viewsets.ModelViewSet):
     #Search feature with filters
     filter_backends = (filters.SearchFilter,)
     search_fields = ('name', 'email',)
+
+
+class UserLoginApiView(ObtainAuthToken):
+    """Handle creating user authentication tokens"""
+    renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES
